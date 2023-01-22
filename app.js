cb7f1c8b5aba9a6aff10c5144a54194178da19fa
Alekzandriia <alekzoreillyz@me.com>
2023-01-22 11:49:43 -0500

    initialize controller

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 // If browser is supported, start app
 if ("webkitSpeechRecognition" in window) {
 
+  // initialize controller
+  let speechRecognition = new webkitSpeechRecognition()
+  speechRecognition.continuous = true // continue listening until user presses stop
+  speechRecognition.interimResults = true //return both final & interim results
+  speechRecognition.lang = 'en-US' // set to english
+  recognition.maxAlternatives = 1 // do not consider alternatives
 
+  
 // if browser is not supported, notify
 } else {
   console.log("Speech Recognition Not Available")
