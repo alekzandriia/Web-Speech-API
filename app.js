d70d04b16fb1740543c4808290c4b205571f4efd
Alekzandriia <alekzoreillyz@me.com>
2023-02-19 20:42:59 -0500

    Final touches
    Change name, add additional styling & footer with link to my website

--- app.js
+++ app.js
@@ -1,17 +1,57 @@
 const start = document.getElementById('start')
 const stop = document.getElementById('stop')
-const output = document.getElementById('output')
+const statusIndicator = document.getElementById('status')
+
+const SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
+const SpeechRecognitionEvent = window.SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
+
 
 // If browser is supported, start app
 if ("webkitSpeechRecognition" in window) {
 
   // initialize controller
-  let speechRecognition = new webkitSpeechRecognition()
+  let speechRecognition = new SpeechRecognition();
   speechRecognition.continuous = true // continue listening until user presses stop
   speechRecognition.interimResults = true //return both final & interim results
   speechRecognition.lang = 'en-US' // set to english
-  recognition.maxAlternatives = 1 // do not consider alternatives
 
+  // Status Indicator
+  start.addEventListener('click', () => {
+    statusIndicator.textContent = "listening..."
+  })
+  stop.addEventListener('click', () => {
+    statusIndicator.textContent = null
+  })
+
+    // final transcript string
+    let final_transcript = ''
+
+  //onResult event
+  speechRecognition.onresult = (event) => {
+    // Create the interim transcript string locally because we don't want it to persist like final transcript
+    let interim_transcript = "";
+
+    // Loop through the results from the speech recognition object.
+    for (let i = event.resultIndex; i < event.results.length; ++i) {
+      // If the result item is Final, add it to Final Transcript, Else add it to Interim transcript
+      if (event.results[i].isFinal) {
+        final_transcript += event.results[i][0].transcript;
+      } else {
+        interim_transcript += event.results[i][0].transcript;
+      }
+    }
+
+    // Set the Final transcript and Interim transcript.
+    document.querySelector("#final").innerHTML = final_transcript;
+    document.querySelector("#interim").innerHTML = interim_transcript;
+  };
+
+  start.addEventListener('click', () => {
+    speechRecognition.start()
+  })
+  stop.addEventListener('click', () => {
+    speechRecognition.stop()
+  })
   
 // if browser is not supported, notify
 } else {
